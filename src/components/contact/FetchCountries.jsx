600ce3d585a7ed55557a2c815f5b75260490755e
1v4n4 <ivananl@yahoo.com>
2022-04-11 19:55:00 +0200

    Fix Macedonia to make @igorkol91 happy :)

--- src/components/contact/FetchCountries.jsx
+++ src/components/contact/FetchCountries.jsx
@@ -6,14 +6,16 @@ const FetchCountries = () => {
   useEffect(() => {
     fetch('https://flagcdn.com/en/codes.json')
       .then((response) => response.json())
-      .then((data) => setCountries(Object.values(data)))
+      .then((data) => setCountries(data))
       .catch((error) => console.error(error));
   }, []);
+
+  countries.mk = 'Macedonia';
 
   /* eslint-disable */
   return (
     <>
-      {countries.map((country, index) =>
+      {Object.values(countries).map((country, index) =>
         (<option key={country + index} value={country}>{country}</option>))}
     </>
   );
