181f45d912e6b1ef9933bf74ae2dc75ca3040e72
1v4n4 <ivananl@yahoo.com>
2022-06-11 21:19:36 +0200

    Correct win footer margin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import isWin from '../helpers';
 import '../CSS/footer.css';
 
 const Footer = () => {
   const location = useLocation();
   const footerClass = location.pathname.match(/projects/) ? 'footer-projects' : 'footer';
+  const winMargin = isWin ? 'mt-5' : '';
+
   return (
     <div className={footerClass}>
-      <p className="text-secondary text-center">
+      <p className={`text-secondary text-center ${winMargin}`}>
         @ 2022
         <a href="https://github.com/1v4n4" className="link-unstyled text-secondary mx-2" target="_blank" rel="noreferrer">1v4n4</a>
       </p>
