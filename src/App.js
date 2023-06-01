81f3483a104cfe959963ceb3921ab2186ba31963
1v4n4 <ivananl@yahoo.com>
2023-06-01 16:09:59 +0300

    Add mui and dark theme

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import Layout from './components/Layout';
 import Intro from './components/Intro';
 import 'react-toastify/dist/ReactToastify.css';
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Intro />} bg="dark" />
-      <Route path="/*" element={<Layout />} bg="dark" />
-    </Routes>
-  </BrowserRouter>
+  <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Intro />} />
+        <Route path="/*" element={<Layout />} />
+      </Routes>
+    </BrowserRouter>
+  </ThemeProvider>
 );
 export default App;
